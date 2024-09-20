d85d46d2458b162cbfd38367110deccfe4064bd1
miller-vrg <millervargas09@gmail.com>
2024-09-19 22:37:54 -0500

    coomit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/**/**/**/**/*.{html,ts}",
+    "./src/app/**/**/**/**/*.{html,ts}",
+    "./src/app/**/**/**/*.{html,ts}",
+    "./src/app/**/**/*.{html,ts}",
+    "./src/app/**/*.{html,ts}",
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
